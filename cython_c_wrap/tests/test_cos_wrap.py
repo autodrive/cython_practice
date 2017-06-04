72dd96687b008895455a7ac5cf3286eb45c5135d
autodrive <kangwon@gmail.com>
2017-06-04 23:27:21 +0900

    added test methods for cos_wrap_ctypes.py

--- cython_c_wrap/tests/test_cos_wrap.py
+++ cython_c_wrap/tests/test_cos_wrap.py
@@ -1,6 +1,9 @@
-import unittest
-import cos_wrap
+import ctypes
 import math
+import unittest
+
+import cos_wrap
+import cos_wrap_ctypes
 
 
 class TestCosWrap(unittest.TestCase):
@@ -18,6 +21,19 @@ class TestCosWrap(unittest.TestCase):
         with self.assertRaises(TypeError):
             cos_wrap.cos_func('foo')
 
+    def test_ctypes_cos(self):
+        angle_deg_range = range(-360, 361)
+        for angle_deg in angle_deg_range:
+            angle_rad = math.radians(angle_deg)
+            expected = math.cos(angle_rad)
+            result = cos_wrap_ctypes.cos_func(angle_rad)
+
+            self.assertAlmostEqual(expected, result,
+                                   msg='angle = %d (deg)' % angle_deg)
+
+    def test_ctypes_cos_wrong_argument(self):
+        with self.assertRaises(ctypes.ArgumentError):
+            cos_wrap_ctypes.cos_func('foo')
 
 if __name__ == '__main__':
     unittest.main()
