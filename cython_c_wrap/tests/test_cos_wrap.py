6ba0f6c099937cc8c5bd8476e025f022ea6e6b92
autodrive <kangwon@gmail.com>
2017-06-05 01:27:13 +0900

    revised test_ctypes_numpy_cos() for ctypes NumPy support

--- cython_c_wrap/tests/test_cos_wrap.py
+++ cython_c_wrap/tests/test_cos_wrap.py
@@ -4,6 +4,8 @@ import unittest
 
 import cos_wrap
 import cos_wrap_ctypes
+import cos_wrap_ctypes_numpy  # ctypes NumPy support
+import numpy as np
 
 
 class TestCosWrap(unittest.TestCase):
@@ -34,6 +36,23 @@ class TestCosWrap(unittest.TestCase):
     def test_ctypes_cos_wrong_argument(self):
         with self.assertRaises(ctypes.ArgumentError):
             cos_wrap_ctypes.cos_func('foo')
+
+    def test_ctypes_numpy_cos(self):
+        angle_deg_array = np.arange(-360, 361)
+        angle_rad_array = np.deg2rad(angle_deg_array)
+        result_array = np.empty_like(angle_rad_array)
+
+        # a little different
+        cos_wrap_ctypes_numpy.cos_doubles_ctypes(angle_rad_array, result_array)
+        expected_array = np.cos(angle_rad_array)
+
+        for angle_deg, result, expected in zip(angle_deg_array, result_array, expected_array):
+            self.assertAlmostEqual(expected, result, msg='angle = %d (deg)' % angle_deg)
+
+    def test_ctypes_numpy_cos_wrong_argument(self):
+        with self.assertRaises(ctypes.ArgumentError):
+            cos_wrap_ctypes_numpy.cos_doubles_ctypes('foo', 'goo')
+
 
 if __name__ == '__main__':
     unittest.main()
