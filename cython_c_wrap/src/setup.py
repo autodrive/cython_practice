912b8c94e26b35b6cbaa67bc36075d199846a364
autodrive <kangwon@gmail.com>
2017-06-04 23:13:59 +0900

    revised distutil setup.py

--- cython_c_wrap/src/setup.py
+++ cython_c_wrap/src/setup.py
@@ -1,10 +1,15 @@
 # Valentin Haenel, 2.8.2. Python-C-Api, Scipy Lectures, Oct 18 2016, [Online]
 #   Available: http://www.scipy-lectures.org/advanced/interfacing_with_c/interfacing_with_c.html#example
+# Valentin Haenel, 2.8.2.2. Numpy Support, Scipy Lectures, Oct 18 2016, [Online]
+#   Available: http://www.scipy-lectures.org/advanced/interfacing_with_c/interfacing_with_c.html#numpy-support
 
 from distutils.core import setup, Extension
 
+import numpy
+
 # define the extension module
-cos_wrap = Extension('cos_wrap', sources=['cos_wrap.c'])
+cos_wrap = Extension('cos_wrap', sources=['cos_wrap.c'],
+                     include_dirs=[numpy.get_include()])
 
 # run the setup
 setup(ext_modules=[cos_wrap])
