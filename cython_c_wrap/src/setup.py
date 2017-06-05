4b1ec59de47cf591ca4e5a032e2b6e960dceba54
autodrive <kangwon@gmail.com>
2017-06-06 04:13:36 +0900

    added one more setup() call line

--- cython_c_wrap/src/setup.py
+++ cython_c_wrap/src/setup.py
@@ -2,8 +2,12 @@
 #   Available: http://www.scipy-lectures.org/advanced/interfacing_with_c/interfacing_with_c.html#example
 # Valentin Haenel, 2.8.4.1. Example, 2.8.4. SWIG, Scipy Lectures, Oct 18 2016, [Online]
 #   Available: , http://www.scipy-lectures.org/advanced/interfacing_with_c/interfacing_with_c.html#id8
+# Valentin Haenel, 2.8.5.1. Example, 2.8.5. Cython, Scipy Lectures, Oct 18 2016, [Online]
+#   Available: , http://www.scipy-lectures.org/advanced/interfacing_with_c/interfacing_with_c.html#id10
 
 from distutils.core import setup, Extension
+
+from Cython.Distutils import build_ext
 
 # define the extension module for Python-C-API
 cos_wrap = Extension('cos_wrap', sources=['cos_wrap.c'])
@@ -13,3 +17,7 @@ cos_wrap_swig = Extension("_cos_wrap_swig", sources=["cos_wrap_swig.c", "cos_wra
 
 # run the setup
 setup(ext_modules=[cos_wrap, cos_wrap_swig])
+
+# for Cython
+setup(cmdclass={'build_ext': build_ext},
+      ext_modules=[Extension("cos_wrap_cython", ['cos_wrap_cython.pyx'])])
