1c3e4e428e23c2d586745ca36b952284e06d1915
KangWon LEE VISSIM user <kangwon.lee@kpu.ac.kr>
2017-06-05 16:28:39 +0900

    added cos_wrap_swig_numpy.h : 2.8.4.2. Numpy Support, 2.8.4. SWIG, http://www.scipy-lectures.org/advanced/interfacing_with_c/interfacing_with_c.html#id9

---
+++ cython_c_wrap/src/cos_wrap_swig_numpy.h
@@ -0,0 +1,5 @@
+/*
+2.8.4.2. Numpy Support, 2.8.4. SWIG, http://www.scipy-lectures.org/advanced/interfacing_with_c/interfacing_with_c.html#id9
+*/
+
+void cos_func_swig_numpy(double * in_array, double * out_array, int size);
