1959259f6fdefed4e1865f6fe09f140eb9e990c5
KangWon LEE VISSIM user <kangwon.lee@kpu.ac.kr>
2017-06-05 13:30:19 +0900

    revised setup.py # 2.8.4.1. Example, 2.8.4. SWIG, http://www.scipy-lectures.org/advanced/interfacing_with_c/interfacing_with_c.html#id8

--- cython_c_wrap/src/setup.py
+++ cython_c_wrap/src/setup.py
@@ -1,10 +1,15 @@
-# Valentin Haenel, 2.8.2. Python-C-Api, Scipy Lectures, Oct 18 2016, [Online]
+# Valentin Haenel, 2.8.2.1. Example, 2.8.2. Python-C-Api, Scipy Lectures, Oct 18 2016, [Online]
 #   Available: http://www.scipy-lectures.org/advanced/interfacing_with_c/interfacing_with_c.html#example
+# Valentin Haenel, 2.8.4.1. Example, 2.8.4. SWIG, Scipy Lectures, Oct 18 2016, [Online]
+#   Available: , http://www.scipy-lectures.org/advanced/interfacing_with_c/interfacing_with_c.html#id8
 
 from distutils.core import setup, Extension
 
-# define the extension module
+# define the extension module for Python-C-API
 cos_wrap = Extension('cos_wrap', sources=['cos_wrap.c'])
 
+# for SWIG
+cos_wrap_swig = Extension("_cos_wrap_swig", sources=["cos_wrap_swig.c", "cos_wrap_swig.i"])
+
 # run the setup
-setup(ext_modules=[cos_wrap])
+setup(ext_modules=[cos_wrap, cos_wrap_swig])
