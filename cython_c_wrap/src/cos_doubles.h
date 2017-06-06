13875c5d089cbfe085e13b8de1e8be3a91db56c6
autodrive <kangwon@gmail.com>
2017-06-06 14:55:43 +0900

    added cos_doubles.h  # 2.8.5.2. Numpy Support, 2.8.5. Cython, http://www.scipy-lectures.org/advanced/interfacing_with_c/interfacing_with_c.html#id13

---
+++ cython_c_wrap/src/cos_doubles.h
@@ -0,0 +1,5 @@
+/*
+    2.8.5.2. Numpy Support, 2.8.5. Cython, http://www.scipy-lectures.org/advanced/interfacing_with_c/interfacing_with_c.html#id13
+*/
+
+void cos_doubles(double * in_array, double * out_array, int size);
